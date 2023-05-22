139e451ef8bd2a65547ba149a065a8831062345d
Aleksey Knyazev <ctj_yebbs@inbox.ru>
2023-05-22 15:18:06 +0300

    2: Package renamed to fit Regulations (#3)
    * Package renamed to fit Regulations
    
    * Version increased

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
-__version = "1.0.1"
+__version = "1.0.2"
 
 spec = {
-    "name": "oc_ldap_client",
+    "name": "oc-ldap-client",
     "version": __version,
     "license": "LGPLv2",
     "description": "Base classes for LDAP objects",
